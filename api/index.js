514e0634d7417fd250b96daf695aefd021ea2864
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-04-21 22:46:05 +0200

    edited index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const path = require("path");
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "images")));
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -32,6 +32,13 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen("8000", () => {
   console.log("Backend is running");
