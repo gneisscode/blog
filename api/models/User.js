9dd4e6a6ffd77e6deef5c6af5f9021806c90583a
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-04-21 22:36:10 +0200

    edited user,js model

--- api/models/User.js
+++ api/models/User.js
@@ -1,31 +1,30 @@
 const mongoose = require("mongoose")
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profilePic: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dosj9cjie/image/upload/v1682109097/default_lvpsoy.png",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-
-  password: {
-    type: String,
-    required: true
-  },
-
-  profilePic: {
-    type: String,
-    default: "../images/default.png",
-
-  },
-
-}, 
-{ timestamps: true}
-
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('User', UserSchema);
