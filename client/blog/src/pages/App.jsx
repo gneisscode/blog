65ae0f67f02b235c75814eacfc814639123be7da
Emmanuella Ikwen <103408439+gneisscode@users.noreply.github.com>
2023-04-21 16:13:12 +0200

    Update App.jsx

--- client/blog/src/pages/App.jsx
+++ client/blog/src/pages/App.jsx
@@ -13,7 +13,7 @@ import { ContextProvider } from '../context/Context'
 import { Context } from "../context/Context";
 
 function App() {
-  axios.defaults.baseURL = `http://localhost:8000/api`;
+  axios.defaults.baseURL = `https://node-react-forum-api.onrender.com/`;
   const {user} = useContext(Context)
 
   return (
